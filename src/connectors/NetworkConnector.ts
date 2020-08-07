49d0c25938694c5920cc192fffbb35736eef85a7
pokim <1325147857@qq.com>
2020-08-07 16:18:18 +0800

    使用新節點

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -58,6 +58,9 @@ class MiniRpcProvider implements AsyncSendable {
     }
     const response = await fetch(this.url, {
       method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
       body: JSON.stringify({
         jsonrpc: '2.0',
         id: 1,
